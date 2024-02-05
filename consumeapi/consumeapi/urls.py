ebcda5aba1936163ee5e94da604e716af0686640
anidxn <anidxn215@gmail.com>
2024-02-05 22:40:52 +0530

    removing folder

--- consumeapi/consumeapi/urls.py
+++ consumeapi/consumeapi/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('getpost.url_getpost'))
+    path('getdata/', views.get_api_data, name='get-data'),
+    path('home/', views.home, name='home'),
+    # path('', include('getpost.url_getpost'))
 ]
